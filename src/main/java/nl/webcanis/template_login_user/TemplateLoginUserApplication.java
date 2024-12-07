d16a391ad7e281f05fa8dba873beabb0a6ed3d7a
avandokkum <arjanvandokkum@gmail.com>
2024-12-07 12:16:19 +0100

    update : Initial commit

---
+++ src/main/java/nl/webcanis/template_login_user/TemplateLoginUserApplication.java
@@ -0,0 +1,13 @@
+package nl.webcanis.template_login_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateLoginUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateLoginUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/webcanis/template_login_user/TemplateLoginUserApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.webcanis.template_login_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateLoginUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
